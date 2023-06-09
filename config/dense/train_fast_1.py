0a7787258bb83baf0e07f146c25a347a177a2be1
Alexander Edwards <alex@alexedw.com>
2023-06-09 16:00:04 +1000

    Crashed run, checking why

--- config/dense/train_fast_1.py
+++ config/dense/train_fast_1.py
@@ -22,6 +22,8 @@ max_iters = 10000
 warmup_iters = 1000
 lr_decay_iters = 10000
 
+train_only = True
+
 ####################  RESUME ####################
 # iter_num = 0
 # load_from_checkpoint = 'alexedw/dense-train-dense-9'
