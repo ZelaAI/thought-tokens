add8cfb0abe1751274f35b1f25c579e68085e556
Alexander Edwards <alex@alexedw.com>
2023-06-25 18:14:13 +1000

    Quick test

--- core/model.py
+++ core/model.py
@@ -198,8 +198,8 @@ class Block(nn.Module):
 class GPTConfig:
     block_size: int = 1024
     vocab_size: int = 50304 # GPT-2 vocab_size of 50257, padded up to nearest multiple of 64 for efficiency
-    n_layer: int = 12
-    n_head: int = 12
+    n_layer: int = 2
+    n_head: int = 8
     n_embd: int = 768
     dropout: float = 0.0
     bias: bool = True # True: bias in Linears and LayerNorms, like GPT-2. False: a bit better and faster
--- core/train.py
+++ core/train.py
@@ -78,8 +78,8 @@ def train(
 
     # Model State
     iter_num = 0,
-    model_config = GPTConfig.from_pretrained('EleutherAI/pythia-410m'),
-    load_from_huggingface = 'EleutherAI/pythia-410m',
+    model_config = GPTConfig(),
+    load_from_huggingface = None,#'EleutherAI/pythia-410m',
     load_from_huggingface_revision = 'main',
     load_from_checkpoint = None,
     load_from_checkpoint_local = False,
