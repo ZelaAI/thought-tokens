8d3a6ec609295b1fb561d987b44f073bb62d3e30
Alexander Edwards <alex@alexedw.com>
2023-07-06 12:26:20 +1000

    Added tests + tinygrad implementation of almost all of rotary embeddings

--- core/model_tiny.py
+++ core/model_tiny.py
@@ -13,4 +13,35 @@ class MLP:
         x = x.gelu()
         x = self.c_proj(x)
         return x
+
+def get_rotary_sin_cos(max_seq_len, config, base=10000):
+    dim = int((config.n_embd // config.n_head) * config.rotary_pct)
+    inv_freq = 1.0 / (base ** (Tensor.arange(start=0, stop=dim, step=2).float() / dim))
     
+    t = Tensor.arange(max_seq_len)
+    freqs = inv_freq.unsqueeze(0) * t.unsqueeze(1)
+    
+    emb = Tensor.cat(freqs, freqs, dim=-1)
+
+    cos = emb.cos()
+    sin = emb.sin()
+    return sin, cos
+
+def rotate_half(x):
+        """Rotates half the hidden dims of the input."""
+        x1 = x[..., : x.shape[-1] // 2]
+        x2 = x[..., x.shape[-1] // 2 :]
+        return Tensor.cat(-x2, x1, dim=-1)    
+
+def rotary_apply(t, sin, cos, rotary_ndims):
+    # Split
+    t_rot, t_pass = t[..., : rotary_ndims], t[..., rotary_ndims :]
+
+    # Apply
+    t_embed = (t_rot * cos) + (rotate_half(t_rot) * sin)
+    # Re-join
+    return Tensor.cat(t_embed, t_pass, dim=-1)    
+
+def apply_rotary_mask(mask, sin_cached, cos_cached):
+    # mask is (B, T)       # sin, cos are (T, D)        # output is (B, 1, T, D)
+    return sin_cached[mask, :].unsqueeze(1), cos_cached[mask, :].unsqueeze(1)
--- tests/test_model.py
+++ tests/test_model.py
@@ -1,8 +1,8 @@
 # First we are going to test the model.
 # Then we're going to rewrite against it in tinygrad.
 import pytest
-from core.model import GPTConfig, GPT, get_rotary_sin_cos, rotary_apply, CausalSelfAttention, MLP, DenseCapture, DenseInject, Block
-from core.model_tiny import MLP as TinyMLP
+from core.model import GPTConfig, GPT, get_rotary_sin_cos, rotary_apply, CausalSelfAttention, MLP, DenseCapture, DenseInject, Block, apply_rotary_mask
+from core.model_tiny import MLP as TinyMLP, get_rotary_sin_cos as tiny_get_rotary_sin_cos, rotary_apply as tiny_rotary_apply, apply_rotary_mask as tiny_apply_rotary_mask
 import torch
 from tinygrad.tensor import Tensor
 import numpy as np
@@ -33,4 +33,47 @@ def test_mlp():
     # Check the output values
     np.testing.assert_allclose(output_tiny.numpy(), output.detach().numpy(), atol=5e-4, rtol=1e-5)
     
+def test_get_rotary_sin_cos():
+    config = GPTConfig()
+    max_seq_len = 1024
+    
+    sin, cos = get_rotary_sin_cos(max_seq_len, config)
+    tiny_sin, tiny_cos = tiny_get_rotary_sin_cos(max_seq_len, config)
+
+    np.testing.assert_allclose(sin.numpy(), tiny_sin.numpy(), atol=5e-4, rtol=1e-5)
+    np.testing.assert_allclose(cos.numpy(), tiny_cos.numpy(), atol=5e-4, rtol=1e-5)    
+
+
+def test_apply_rotary_mask():
+    pos_mask = torch.arange(1024, dtype=torch.long).unsqueeze(0)
+    pos_mask_tiny = Tensor(pos_mask.numpy())
+    
+    sin_cached, cos_cached = get_rotary_sin_cos(1024, GPTConfig())
+    tiny_sin_cached, tiny_cos_cached = Tensor(sin_cached.numpy()), Tensor(cos_cached.numpy())
+    
+    sin, cos = apply_rotary_mask(pos_mask, sin_cached, cos_cached)
+    tiny_sin, tiny_cos = tiny_apply_rotary_mask(pos_mask_tiny, tiny_sin_cached, tiny_cos_cached)
+    
+    np.testing.assert_allclose(sin.numpy(), tiny_sin.numpy(), atol=5e-4, rtol=1e-5)
+    np.testing.assert_allclose(cos.numpy(), tiny_cos.numpy(), atol=5e-4, rtol=1e-5)
+
+    
+    
+def test_rotary_apply():
+    batch_size, n_head, seq_length, head_size, rotary_pct = 4, 8, 1024, 128, 0.25
+    rotary_ndims = int(head_size * rotary_pct)
+    sin_cached, cos_cached = get_rotary_sin_cos(seq_length, GPTConfig(n_embd=n_head*head_size, n_head=n_head, rotary_pct=rotary_pct))
+    
+    sin, cos = apply_rotary_mask(torch.arange(seq_length, dtype=torch.long).unsqueeze(0), sin_cached, cos_cached) 
+    tiny_sin, tiny_cos = Tensor(sin.numpy()), Tensor(cos.numpy())
+    
+    x = torch.randn(batch_size, n_head, seq_length, head_size)
+    tiny_x = Tensor(x.numpy())
+    
+    # Pass the input through the rotary apply
+    output = rotary_apply(x, sin, cos, rotary_ndims)
+    output_tiny = tiny_rotary_apply(tiny_x, tiny_sin, tiny_cos, rotary_ndims)
+    
+    # Check the output values
+    np.testing.assert_allclose(output_tiny.numpy(), output.detach().numpy(), atol=5e-4, rtol=1e-5)
     
