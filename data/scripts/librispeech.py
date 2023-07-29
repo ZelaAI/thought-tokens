9d779b7ccf351321b12fae01b77e04671eb26886
Alexander Edwards <alex@alexedw.com>
2023-07-29 13:18:49 +1000

    Add (messy) multiprocessing too tokenize to run locally.

---
+++ data/scripts/librispeech.py
@@ -0,0 +1,65 @@
+from datasets import Dataset
+import datasets
+import os
+
+def _generate_examples(files, local_extracted_archive):
+    """Generate examples from a LibriSpeech archive_path."""
+    key = 0
+    audio_data = {}
+    transcripts = []
+    for path in files:
+        with open(path, "rb") as f:
+            if path.endswith(".flac"):
+                id_ = path.split("/")[-1][: -len(".flac")]
+                audio_data[id_] = f.read()
+            elif path.endswith(".trans.txt"):
+                for line in f:
+                    if line:
+                        line = line.decode("utf-8").strip()
+                        id_, transcript = line.split(" ", 1)
+                        audio_file = f"{id_}.flac"
+                        speaker_id, chapter_id = [int(el) for el in id_.split("-")[:2]]
+                        audio_file = (
+                            os.path.join(local_extracted_archive, audio_file)
+                            if local_extracted_archive
+                            else audio_file
+                        )
+                        transcripts.append(
+                            {
+                                "id": id_,
+                                "speaker_id": speaker_id,
+                                "chapter_id": chapter_id,
+                                "file": audio_file,
+                                "text": transcript,
+                            }
+                        )
+            if audio_data and len(audio_data) == len(transcripts):
+                for transcript in transcripts:
+                    audio = {"path": transcript["file"], "bytes": audio_data[transcript["id"]]}
+
+                    yield {"audio": audio, **transcript}
+                    key += 1
+                    
+                    if key == 100: break
+                audio_data = {}
+                transcripts = []
+
+def _dataset_generator():
+    def files_generator():
+        for root, dirs, filenames in os.walk("data.ignore/LibriSpeech"):
+            for filename in filenames:
+                yield os.path.join(root,filename)
+
+    return _generate_examples(files_generator(), None)
+
+def get_dataset():
+    return Dataset.from_generator(_dataset_generator, features=datasets.Features(
+        {
+            "file": datasets.Value("string"),
+            "audio": datasets.Audio(sampling_rate=16_000),
+            "text": datasets.Value("string"),
+            "speaker_id": datasets.Value("int64"),
+            "chapter_id": datasets.Value("int64"),
+            "id": datasets.Value("string"),
+        }
+    ))
--- data/scripts/tokenize_audio_dataset.py
+++ data/scripts/tokenize_audio_dataset.py
@@ -1,4 +1,6 @@
 import sys
+
+from data.scripts.librispeech import get_dataset
 sys.path.append('.')
 from datasets import load_dataset, Audio as AudioHF
 from data.packer import Packer
@@ -9,21 +11,13 @@ import random
 from datasets import Dataset
 from transformers import GPTNeoXTokenizerFast
 from core.tokenizer import Tokenizer, AudioTokenizer
+from multiprocessing import Pool, cpu_count
 
 max_seq_len = 2048 + 1 # account for input/target offset
 num_workers = 4
-source_dataset_repo_id = "hf-internal-testing/librispeech_asr_dummy"
-source_dataset_split = 'clean'
-destination_dataset_repo_id = "ZelaAI/librispeech_tiny_2048"
+destination_dataset_repo_id = "ZelaAI/librispeech_clean_100_2048"
 
-print("Loading tokenizer...")
-tokenizer = Tokenizer()
-audio_tokenizer = AudioTokenizer()
-print("Loading dataset...")
-dataset = load_dataset(source_dataset_repo_id, source_dataset_split)
-dataset = dataset.cast_column("audio", AudioHF(sampling_rate=24000))
-
-def tokenize(example):
+def tokenize(example, tokenizer, audio_tokenizer):
     text_tokenized = tokenizer.encode(example['text'].lower()).numpy()
     audio_tokenized = audio_tokenizer.encode(example['audio']['array']).numpy()
 
@@ -50,77 +44,105 @@ def tokenize(example):
         text_tokens = np.concatenate([construct_text_text_tokens, construct_audio_text_tokens])
         audio_tokens_1 = np.concatenate([construct_text_audio_tokens, construct_audio_audio_tokens_1])
         audio_tokens_2 = np.concatenate([construct_text_audio_tokens, construct_audio_audio_tokens_2])
-        
+    
     return {
         'text_tokens': text_tokens,
         'audio_tokens_1': audio_tokens_1,
         'audio_tokens_2': audio_tokens_2,
+        'length': len(text_tokens),
     }
 
+def chunked_map(func, data, num_processes=None):
+    if num_processes is None:
+        num_processes = cpu_count()
+    print(f"Using {num_processes} processes out of {cpu_count()} available")
 
-print("Tokenizing dataset...")
-tokenized_dataset = dataset.map(tokenize)
-tokenized_dataset.set_format(type='numpy', columns=['text_tokens', 'audio_tokens_1', 'audio_tokens_2'])
+    chunk_size = len(data) // num_processes
+    chunks = [data.select(range(i, min(i + chunk_size, len(data)))) for i in range(0, len(data), chunk_size)]
+    print(f"Splitting data into {len(chunks)} chunks")
+    print(f"Chunk sizes: {[len(chunk) for chunk in chunks]}")
 
-def length(example):
-    return {
-        'length': len(example['text_tokens']),
-    }
+    results = []
+    with Pool(processes=num_processes) as pool:
+        for result_chunk in tqdm(pool.imap_unordered(func, chunks), total=len(chunks)):
+            results.extend(result_chunk)
 
-with_lengths = tokenized_dataset.map(length)
-filtered = with_lengths.filter(lambda example: example['length'] <= max_seq_len)
+    return results
 
-## Packing the dataset
-# We don't naively just concat examples together, as that leads to an unnecessary amount of examples sitting on boundaries
-# Packing is much more effective.
+def tokenize_chunk(chunk):
+    print("Loading tokenizer...")
+    tokenizer = Tokenizer()
+    audio_tokenizer = AudioTokenizer()
+    print("Loaded audio tokenizer")
+    return [tokenize(example, tokenizer, audio_tokenizer) for example in tqdm(chunk)]
 
-@dataclass
-class TrainingSequence:
-    id: int
-    length: int
-    text_tokens: np.array
-    audio_tokens_1: np.array
-    audio_tokens_2: np.array
 
-    def __lt__(self, other):
-        # No-op
-        return False
+if __name__ == '__main__':
+    print("Loading dataset...")
+    dataset = get_dataset()
+    dataset = dataset.cast_column("audio", AudioHF(sampling_rate=24000))
 
-print("Preparing dataset for packing...")
-training_sequences = []
+    print("Tokenizing dataset...")
+    tokenized_data = chunked_map(tokenize_chunk, dataset, num_processes=4)
 
-for i, example in tqdm(enumerate(filtered['train'])):
-    training_sequences.append(TrainingSequence(
-        id=i,
-        length=example['length'],
-        text_tokens=example['text_tokens'],
-        audio_tokens_1=example['audio_tokens_1'],
-        audio_tokens_2=example['audio_tokens_2'],
-    ))
+    tokenized_dataset = Dataset.from_list(tokenized_data)
+    print(tokenized_dataset)
 
-print("Packing dataset...")
-packer = Packer(max_seq_len, training_sequences)
-packed_sequences = packer.to_list()
-print(packer)
+    tokenized_dataset.set_format(type='numpy', columns=['text_tokens', 'audio_tokens_1', 'audio_tokens_2'], output_all_columns=True)
 
-print("Shuffling dataset...")
-random.shuffle(packed_sequences)
+    filtered = tokenized_dataset.filter(lambda example: example['length'] <= max_seq_len)
 
-packed_text_tokens = []
-packed_audio_tokens_1 = []
-packed_audio_tokens_2 = []
+    ## Packing the dataset
+    # We don't naively just concat examples together, as that leads to an unnecessary amount of examples sitting on boundaries
+    # Packing is much more effective.
 
-print("Concatenating dataset...")
-for packed_sequence in tqdm(packed_sequences):
-    packed_text_tokens.append(np.concatenate([seq.text_tokens for seq in packed_sequence]))
-    packed_audio_tokens_1.append(np.concatenate([seq.audio_tokens_1 for seq in packed_sequence]))
-    packed_audio_tokens_2.append(np.concatenate([seq.audio_tokens_2 for seq in packed_sequence]))
+    @dataclass
+    class TrainingSequence:
+        id: int
+        length: int
+        text_tokens: np.array
+        audio_tokens_1: np.array
+        audio_tokens_2: np.array
 
-print("Pushing dataset to HuggingFace Hub...")
-final_dataset = Dataset.from_dict({
-    'text_tokens': packed_text_tokens,
-    'audio_tokens_1': packed_audio_tokens_1,
-    'audio_tokens_2': packed_audio_tokens_2,
-})
+        def __lt__(self, other):
+            # No-op
+            return False
 
-final_dataset.push_to_hub(destination_dataset_repo_id)
+    print("Preparing dataset for packing...")
+    training_sequences = []
+
+    for i, example in tqdm(enumerate(filtered)):
+        training_sequences.append(TrainingSequence(
+            id=i,
+            length=example['length'],
+            text_tokens=example['text_tokens'],
+            audio_tokens_1=example['audio_tokens_1'],
+            audio_tokens_2=example['audio_tokens_2'],
+        ))
+
+    print("Packing dataset...")
+    packer = Packer(max_seq_len, training_sequences)
+    packed_sequences = packer.to_list()
+    print(packer)
+
+    print("Shuffling dataset...")
+    random.shuffle(packed_sequences)
+
+    packed_text_tokens = []
+    packed_audio_tokens_1 = []
+    packed_audio_tokens_2 = []
+
+    print("Concatenating dataset...")
+    for packed_sequence in tqdm(packed_sequences):
+        packed_text_tokens.append(np.concatenate([seq.text_tokens for seq in packed_sequence]))
+        packed_audio_tokens_1.append(np.concatenate([seq.audio_tokens_1 for seq in packed_sequence]))
+        packed_audio_tokens_2.append(np.concatenate([seq.audio_tokens_2 for seq in packed_sequence]))
+
+    print("Pushing dataset to HuggingFace Hub...")
+    final_dataset = Dataset.from_dict({
+        'text_tokens': packed_text_tokens,
+        'audio_tokens_1': packed_audio_tokens_1,
+        'audio_tokens_2': packed_audio_tokens_2,
+    })
+
+    final_dataset.push_to_hub(destination_dataset_repo_id)
