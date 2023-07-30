f99e85277ae18a3d20e63a9136e737019930db5e
Alexander Edwards <alex@alexedw.com>
2023-07-30 12:02:37 +1000

    Modify `create_stream_dataset` to accept multiple input datasets

--- data/scripts/create_stream_dataset.py
+++ data/scripts/create_stream_dataset.py
@@ -3,20 +3,19 @@ This script loads a Huggingface Dataset, manually shards it, and uploads it to a
 
 For use with `data/stream_dataset.py`
 """
-source_dataset_repo_id = "ZelaAI/librispeech_clean_100_2048"
-num_shards = 10
-destination_dataset_repo_id = "ZelaAI/librispeech_clean_100_2048_streamable"
+source_datasets_repo_ids = ["ZelaAI/librispeech_clean_100_2048", "ZelaAI/librispeech_clean_360_2048"]
+num_shards = 30
+destination_dataset_repo_id = "ZelaAI/librispeech_clean_2048_streamable"
 
 import os
 import math
 from huggingface_hub import HfApi
 import json
-from datasets import load_dataset
+from datasets import concatenate_datasets, load_dataset
 
 print("Loading dataset...")
-dataset_sample = load_dataset(source_dataset_repo_id, split="train")
-
-dataset_length = len(dataset_sample)
+dataset = concatenate_datasets([load_dataset(v, split="train") for v in source_datasets_repo_ids])
+dataset_length = len(dataset)
 
 config = {
     "num_shards": num_shards,
@@ -34,7 +33,7 @@ for shard_id in range(num_shards):
     end = min((shard_id + 1) * shard_size, dataset_length)
     
     # Save as a parquet file
-    dataset_sample.select(range(start, end)).to_parquet(f"dataset_upload.ignore/shard_{shard_id}.parquet")
+    dataset.select(range(start, end)).to_parquet(f"dataset_upload.ignore/shard_{shard_id}.parquet")
 
 api = HfApi()
 
